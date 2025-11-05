c9c9c28db02335f094ba1702b0ad9e091a959f0f
Akashmesh <akashmeshram2486@gmail.com>
2025-11-06 00:52:02 +0530

    jwttoken verification

--- server/Router/auth-router.js
+++ server/Router/auth-router.js
@@ -1,5 +1,6 @@
 const express =require("express");
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
 
 const authcontrollers= require("../Controllers/auth-controller");
 const {signupSchema, loginSchema} = require("../validators/auth-validators");
@@ -8,6 +9,6 @@ const validate = require("../middlewares/validate-middleware");
 router.route("/").get(authcontrollers.home);
 router.route("/register").post(validate(signupSchema),authcontrollers.register);
 router.route("/login").post(validate(loginSchema),authcontrollers.login);
-
+router.route("/user").get(authMiddleware, authcontrollers.user);
 
 module.exports= router;
