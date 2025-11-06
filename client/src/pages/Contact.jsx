ada9952652a5df4eff82473c4f7c53e2e6f6172b
Akashmesh <akashmeshram2486@gmail.com>
2025-11-07 01:22:20 +0530

    stored the user message in mongo db and fetched username to about page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -27,10 +27,32 @@ export const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit =async (e) => {
     e.preventDefault();
 
     console.log(contact);
+    try {
+      const response  = await fetch("http://localhost:5000/api/form/contact", {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(contact),
+      });
+      if(response.ok) {
+        setContact({
+          username : "",
+          email : "",
+          message : "",
+        });
+        const data = await response.json();
+        console.log(data);
+        alert("message sent successfully");
+      }
+    } catch (error) {
+      alert("message not sent");
+      console.log(error);
+    }
   };
 
 
